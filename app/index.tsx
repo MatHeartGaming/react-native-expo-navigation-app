72eb91ff9984514173ed57093184d327d3f3f71d
Matteo Buompastore <matteo.buompastore088@gmail.com>
2025-05-07 15:28:00 +0200

    Added fonts and custom colors to tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -6,10 +6,18 @@ const App = () => {
         <SafeAreaView>
             <View className='mt-6 mx-2.5'>
                 <Text className='text-5xl font-bold text-blue-500 text-center'>Hola Mundo</Text>
-                <Text className='text-4xl' style={{ fontFamily: 'WorkSans-Black' }}>Hola Mundo</Text>
-                <Text className='text-3xl font-work-black'>Hola Mundo</Text>
-                <Text className='text-2xl font-work-medium'>Hola Mundo</Text>
-                <Text className='text-1xl font-work-light'>Hola Mundo</Text>
+                <Text className='text-4xl text-primary' style={{ fontFamily: 'WorkSans-Black' }}>
+                    4XL Primary Color Work Black
+                </Text>
+                <Text className='text-3xl text-secondary font-work-black'>
+                    3XL Secondary Color Work Black
+                </Text>
+                <Text className='text-2xl text-secondary-200 font-work-medium'>
+                    2XL Secondary-200 Color Work Medium
+                </Text>
+                <Text className='text-1xl text-tertiary font-work-light'>
+                    1XL Tertiary Color Work Light
+                </Text>
             </View>
         </SafeAreaView>
     )
