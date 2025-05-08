26f3be655d26b0546226470feb36db62225fddf2
Matteo Buompastore <matteo.buompastore088@gmail.com>
2025-05-08 12:01:32 +0200

    Customised Drawer

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,14 +1,28 @@
+import CustomDrawer from '@/components/shared/CustomDrawer';
+import { Ionicons } from '@expo/vector-icons';
 import { Drawer } from 'expo-router/drawer';
 import React from 'react';
 
 const DrawerLayout = () => {
   return (
-    <Drawer>
+    <Drawer
+        /// Using drawerContent this will ignore Drawer.Screen screens
+        drawerContent={ CustomDrawer }
+        screenOptions={{
+            overlayColor: 'rgba(0, 0, 0, 0.4)',
+            drawerActiveTintColor: 'indigo',
+            headerShadowVisible: false,
+            sceneStyle: {
+                backgroundColor: 'white'
+            },
+        }}
+    >
         <Drawer.Screen
           name="user/index" // This is the name of the page and must match the url from root
           options={{
             drawerLabel: 'Usuario',
             title: 'Usuario',
+            drawerIcon: ({color, size}) => <Ionicons name='person-circle-outline' color={color} size={size} />
           }}
         />
         <Drawer.Screen
@@ -16,6 +30,7 @@ const DrawerLayout = () => {
           options={{
             drawerLabel: 'Horarios',
             title: 'Horarios',
+            drawerIcon: ({color, size}) => <Ionicons name='calendar-outline' color={color} size={size} />
           }}
         />
       </Drawer>
